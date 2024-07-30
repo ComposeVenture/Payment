e56bf8fc8214b8c8c354ddb4a1be3539cc099d7a
ComposeVenture <composeventures@gmail.com>
2024-07-30 13:57:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+DISPLAY('',`
+
+    <div class='HomeDiv'>
+
+        <img class='AppLogo' src='./Images/invest.jpg'/>
+
+        <div class='MessagingDiv'>
+
+            <h3>Money Building</h3>
+
+            <p>Your Money Growing Platform</p>
+       
+       </div>
+
+       <div class='Image'>
+
+            <img class='Invest2' src='./Images/bs.png'/>
+       
+       </div>
+
+       <p class='SimpleMessage'>Increase Your Earning Today With Us</p>
+
+       <button class='forestgreen'>Invest Now</button>
+
+       
+    
+    </div>
+
+    <header>
+
+        <h1 class='AppName'>LacerTradex</h1>
+
+        <img  class='RightedIcon' src='./WhiteIcons/menu.png'/>
+    
+    </header>
+
+`);
