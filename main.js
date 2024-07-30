3262ac652a05d208fb704575f3f534b84746e5d6
ComposeVenture <composeventures@gmail.com>
2024-07-30 17:24:47 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -22,6 +22,53 @@ DISPLAY('',`
 
        <button class='forestgreen'>Invest Now</button>
 
+       <div  class='relativediv'>
+
+            <h1>About Us</h1>
+
+            <p>We are a investiment platform that is aimed to double your income house hold and return profits to you and add value to your money.</p>
+            <p>Lacer Fund Uses your money in real estate investiment and we grantee you a return investiment of 7% on your money and withdrawal of your money takes only 24 hours of working days that is a income gross rate and non taxable capital in your pocket </p>
+
+            <div class='divholder'>
+
+                <p class='Floaters'>Packages</p>
+
+                <p class='Floaters'>Withdrawal<p>
+
+                <p class='Floaters'>Balance<p>
+            
+            </div>
+
+            <h1 class='Floaters'>Get Started</h1>
+
+            <p>To Start Invseting With Us Today,Create An Account Inorder to Keep Track OF Your Invsestiments and returns for a better profit</p>
+
+            <div class='divholder'>
+
+                <p class='Floaters'>Login<p>
+
+                <p class='Floaters'>Create Account</p>
+             
+            </div>
+
+            <h3>COntact Us Today</h3>
+
+            <p>For More Inquiries Please Contact Us Via </p>
+
+            <div class='divholder'>
+
+                <p class='Floaters'>Email</p>
+
+                <p class='Floaters'>WhatsApp<p>
+
+                <p class='Floaters'>FaceBook<p>
+            
+            </div>
+
+            <p>@  Lacer Fund 2024</p>
+       
+       </div>
+
     </div>
 
     <header>
